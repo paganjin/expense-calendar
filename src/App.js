283be82b9eed9dce65cf816907f8c8f5b4b04c24
Tianyou JIN <ohjinjinjin@gmail.com>
2021-09-04 01:18:52 +1000

    redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-// import SimpleInput from './components/SimpleInput';
-import BasicForm from './components/BasicForm';
+import { Fragment } from 'react'
+import { useSelector } from 'react-redux'
+
+import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+
 
 function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated)
+
   return (
-    <div className="app">
-      <BasicForm />
-    </div>
+    <Fragment>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+      <Counter />
+    </Fragment>
   );
 }
 
