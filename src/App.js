52c8fc6876c979b94b0a69d9be1c94dda5b5a75c
Tianyou JIN <ohjinjinjin@gmail.com>
2021-09-04 14:34:10 +1000

    redux-practice

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import { Fragment } from 'react'
-import { useSelector } from 'react-redux'
+import { Fragment, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
 
-import Counter from './components/Counter';
-import Header from './components/Header';
-import Auth from './components/Auth';
-import UserProfile from './components/UserProfile';
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification'
+import { sendCartData, fetchCartData } from './store/cart-actions'
 
+let isInitial = true;
 
 function App() {
-  const isAuth = useSelector(state => state.auth.isAuthenticated)
+  const dispatch = useDispatch()
+  const showCart = useSelector(state => state.ui.cartIsVisible)
+  const cart = useSelector(state => state.cart)
+  const notification = useSelector(state => state.ui.notification)
+
+  useEffect(() => {
+    dispatch(fetchCartData())
+  }, [dispatch])
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false
+      return
+    }
+
+    if(cart.changed) {
+      dispatch(sendCartData(cart))
+    }
+  }, [cart, dispatch])
 
   return (
     <Fragment>
-      <Header />
-      {!isAuth && <Auth />}
-      {isAuth && <UserProfile />}
-      <Counter />
+    {notification && 
+      <Notification 
+        status={notification.status}
+        title={notification.title}
+        message={notification.message}
+      />}
+    <Layout>
+      {showCart && <Cart />}
+      <Products />
+    </Layout>
     </Fragment>
   );
 }
