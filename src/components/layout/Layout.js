d70f896100670c668bb33dd9054742684d8386c9
Tianyou JIN <ohjinjinjin@gmail.com>
2021-09-09 20:40:00 +1000

    authentication

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,15 +1,14 @@
-import { Fragment } from 'react'
+import { Fragment } from 'react';
 
-import classes from './Layout.module.css'
-import MainNavigation from './MainNavigation'
+import MainNavigation from './MainNavigation';
 
 const Layout = (props) => {
   return (
     <Fragment>
       <MainNavigation />
-      <main className={classes.main}>{props.children}</main>
+      <main>{props.children}</main>
     </Fragment>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
