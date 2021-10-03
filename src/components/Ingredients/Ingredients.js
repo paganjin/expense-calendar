699140702e254af230d491aabffa13cb37e29e01
Tianyou JIN <ohjinjinjin@gmail.com>
2021-10-03 20:39:21 +1100

    refresh react-hooks

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useCallback, useReducer, useMemo } from 'react';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
+import Search from './Search';
+import useHttps from '../../hooks/http';
+
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients
+    case 'ADD':
+      return [...currentIngredients, action.ingredient]
+    case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id)
+    default:
+      throw new Error('Should not get there.')
+  }
+}
+
+const Ingredients = () => {
+  const [ userIngredients, dispatch ] = useReducer(ingredientReducer, [])
+  const { isLoading, error, data, sendRequest, reqExtra, reqIdentifier, clear } = useHttps()
+
+  useEffect(() => {
+    if (!isLoading && !error &&reqIdentifier === 'REMOVE_INGREDIENT') {
+      dispatch({ type: 'DELETE', id: reqExtra })
+    } else if (!isLoading && !error && reqIdentifier === 'ADD_INGREDIENT') {
+      dispatch({
+        type: 'ADD',
+        ingredient: { id: data.name, ...reqExtra }
+      })
+    }
+  }, [data, reqExtra, reqIdentifier, isLoading, error])
+
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    dispatch({
+      type: 'SET',
+      ingredients: filteredIngredients
+    })
+  }, [])
+
+  const addIngredientHandler = useCallback(ingredient => {
+    sendRequest(
+      'https://react-http-aa5f1-default-rtdb.firebaseio.com/ingredients.json',
+      'POST',
+      JSON.stringify(ingredient),
+      ingredient,
+      'ADD_INGREDIENT'
+    )
+  }, [sendRequest])
+
+  const removeItemHandler = useCallback(ingredientId => {
+    sendRequest(
+      `https://react-http-aa5f1-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`,
+      'DELETE',
+      null,
+      ingredientId,
+      'REMOVE_INGREDIENT'
+    )
+  }, [sendRequest])
+
+  const ingredientList = useMemo(() => {
+    return(
+      <IngredientList
+        ingredients={userIngredients}
+        onRemoveItem={removeItemHandler}
+      />
+    )
+  }, [userIngredients, removeItemHandler])
+
+  return (
+    <div className="App">
+      {error && <ErrorModal onClose={clear} >{error}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading} />
+      <section>
+        <Search onLoadIngredients={filteredIngredientsHandler} />
+        {ingredientList}
+      </section>
+    </div>
+  );
+}
+
+export default Ingredients;
